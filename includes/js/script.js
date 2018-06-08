84b2355f686ef571eeb2427bbaede92ed52e4ae5
nettaricher <nettaricher1@gmail.com>
2018-06-08 23:46:18 +0300

    responsive take 2

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,3 +1,4 @@
+var flag = 0;
 (function(){
 	var obj1 = document.getElementById("add1");
 	var obj2 = document.getElementById("add-form");
@@ -5,3 +6,17 @@
 		obj2.style.display = "block";
 	}
 })();
+function myFunction(x) {
+	if (flag == 0){
+	    x.classList.toggle("change");
+	    document.getElementById("sideMenu").style.display = "block";
+	    document.getElementById("hamburger").style.right = "190px";
+	    flag = 1;
+	}
+	else {
+		x.classList.toggle("change");
+	    document.getElementById("sideMenu").style.display = "none";
+	    document.getElementById("hamburger").style.right = "0";
+	    flag = 0;
+	}
+}
